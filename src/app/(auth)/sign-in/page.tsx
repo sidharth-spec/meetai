4327fe7df9f45043c4871ed95b2d9773b80b7d3e
sidharth-spec <siddharthjaiswal717@gmail.com>
2025-09-27 20:15:53 +0530

    04:Authentication ui

---
+++ src/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,7 @@
+import { SignInViews } from "@/modules/auth/ui/views/sign-in-views";
+
+const page = () => {
+  return <SignInViews />;
+};
+
+export default page;
---
+++ src/app/(auth)/sign-up/page.tsx
@@ -0,0 +1,8 @@
+import { SignUpViews } from "@/modules/auth/ui/views/sign-up-views";
+import React from "react";
+
+const page = () => {
+  return <SignUpViews />;
+};
+
+export default page;
