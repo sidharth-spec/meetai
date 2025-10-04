b085b605c5d1e8cfda5e173b042d551adabec88d
sidharth-spec <siddharthjaiswal717@gmail.com>
2025-10-04 13:45:00 +0530

    09:agents setup

--- src/modules/dashboard/ui/components/dashboard-sidebar.tsx
+++ src/modules/dashboard/ui/components/dashboard-sidebar.tsx
@@ -55,7 +55,7 @@ export const DashBoardSidebar = () => {
           <SidebarGroupContent>
             <SidebarMenu>
               {firstSection.map((item) => (
-                <SidebarMenuItem>
+                <SidebarMenuItem key={item.href}>
                   <SidebarMenuButton
                     className={cn(
                       "h-10 hover:bg-linear-to-r/oklch border border-transparent hover:border-[#5D6B68]/10 from-sidebar-accent from-5% via-30% via-sidebar/50 to-sidebar/50",
@@ -83,7 +83,7 @@ export const DashBoardSidebar = () => {
           <SidebarGroupContent>
             <SidebarMenu>
               {secondSection.map((item) => (
-                <SidebarMenuItem>
+                <SidebarMenuItem key={item.href}>
                   <SidebarMenuButton
                     className={cn(
                       "h-10 hover:bg-linear-to-r/oklch border border-transparent hover:border-[#5D6B68]/10 from-sidebar-accent from-5% via-30% via-sidebar/50 to-sidebar/50",
