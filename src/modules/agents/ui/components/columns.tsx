e499087fdb23e62c9d561093bc8bf371b56c457c
sidharth-spec <siddharthjaiswal717@gmail.com>
2025-10-06 18:57:10 +0530

    13:agents filter

--- src/modules/agents/ui/components/columns.tsx
+++ src/modules/agents/ui/components/columns.tsx
@@ -38,7 +38,9 @@ export const columns: ColumnDef<AgentGetOne>[] = [
         variant="outline"
         className="flex items-center gap-x-2 [&>svg]:size-4"
       >
-        <VideoIcon className="text-blue-500" />5 Meetings
+        <VideoIcon className="text-blue-500" />
+        {row.original.meetingCount}{" "}
+        {row.original.meetingCount === 1 ? "meeting" : "meetings"}
       </Badge>
     ),
   },
