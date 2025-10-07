803ccb342582721a0a3d3330ee910ffa345e07af
sidharth-spec <siddharthjaiswal717@gmail.com>
2025-10-07 19:04:12 +0530

    14: agents page

---
+++ src/app/(dashboard)/agents/[agentId]/page.tsx
@@ -0,0 +1,30 @@
+import { getQueryClient, trpc } from "@/trpc/server";
+import { HydrationBoundary, dehydrate } from "@tanstack/react-query";
+import { ErrorBoundary } from "react-error-boundary";
+import { Suspense } from "react";
+import {
+  AgentIdView,
+  AgentIdViewError,
+  AgentsIdViewLoading,
+} from "@/modules/agents/ui/views/agent-id-view";
+
+interface Props {
+  params: Promise<{ agentId: string }>;
+}
+const Page = async ({ params }: Props) => {
+  const { agentId } = await params;
+  const queryClient = getQueryClient();
+  void queryClient.prefetchQuery(
+    trpc.agents.getOne.queryOptions({ id: agentId })
+  );
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <Suspense fallback={<AgentsIdViewLoading />}>
+        <ErrorBoundary fallback={<AgentIdViewError />}>
+          <AgentIdView agentId={agentId} />
+        </ErrorBoundary>
+      </Suspense>
+    </HydrationBoundary>
+  );
+};
+export default Page;
